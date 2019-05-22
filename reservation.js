aedd6c4388b8ac1150387ab72f7542e199b19389
treyarnold <arnold.trey80@gmail.com>
2019-05-21 21:36:26 -0500

    get routes set

--- reservation.js
+++ reservation.js
@@ -1,6 +1,6 @@
 //const uuidv5 = require('uuid/v5');
 
-const Reservation = function ({ name, phoneNumber, email, id }) {
+const Reservation = function ( name, phoneNumber, email, id ) {
   this.name = name;
   this.phoneNumber = phoneNumber;
   this.email = email;
